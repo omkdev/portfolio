d731ae30670e75688b4fcce83d6a141e9dbaa623
Om Mahesh Kanse <omkanse1010@gmail.com>
2026-05-31 13:03:40 +0530

    updated main file

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css";
 import posthog from "posthog-js";
 import * as Sentry from "@sentry/react";
 import { Analytics } from "@vercel/analytics/react";
